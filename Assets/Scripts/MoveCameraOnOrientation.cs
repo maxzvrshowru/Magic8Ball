ace5af9eb20a1d38d429b3c46732f2f9fd054447
Максим Завьялов <max.z@vrshow.ru>
2019-09-19 12:03:13 +0300

    Первая версия Магического шарика, работает на андроид

---
+++ Assets/Scripts/MoveCameraOnOrientation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraOnOrientation : MonoBehaviour
+{
+    private DeviceOrientation deviceOrientation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deviceOrientation = Input.deviceOrientation;
+        changeOrientation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.deviceOrientation != deviceOrientation) Start();
+    }
+
+    private void changeOrientation()
+    {
+        if (deviceOrientation == DeviceOrientation.LandscapeLeft || deviceOrientation == DeviceOrientation.LandscapeRight)
+        {
+            transform.position = new Vector3(0, 0, -1);
+        }
+
+        if (deviceOrientation == DeviceOrientation.Portrait || deviceOrientation == DeviceOrientation.PortraitUpsideDown || deviceOrientation == DeviceOrientation.FaceUp )
+        {
+            transform.position = new Vector3(0, 0, -2);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomAnswer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomAnswer : MonoBehaviour
+{
+    
+    public GameObject ansverTriangle; // Ссылка на треугольник в сцене
+    public Texture[] ansvers; // Массив текстур с ответами
+
+    private float previousTime; // Начало старта анимации
+    private bool getRandomAnsver = false; // Получить рандомный ответ
+
+    // Это устаревший класс-компонент (Legacy), но он несколько проще в понимании, чем Animator
+    private Animation aminRotation; 
+
+    void Start()
+    {
+        // Получаем ссылку на компонент анимации, прикрепленный к шарику
+        aminRotation = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        // Если нажата кнопка мышки или пробел, и анимация не проигрывается
+        // Важный пример оптимизации и рефакторинга
+        // Проверка аргументов идет от левого к правому
+        // В начале проверки лучше проверить самое простое условие, например
+        // В нашем случае это флаг, что анимация не проигрывается
+        // Такая проверка происходит намного быстрее, чем проверка на нажатие
+        if (!aminRotation.isPlaying && (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)))
+        {
+            previousTime = Time.time; // Устанавливаем текущее время
+            aminRotation.Play(); // Запускаем анимацию
+            getRandomAnsver = true; // Нунжно получить ответ
+        }
+
+        // Получить рандомный ответ через полсекунды от начала анимации
+        // getRandomAnsver проверяется первым
+        if (getRandomAnsver && Time.time - previousTime > 0.5f)
+        {
+            // Мы меняем текстуру на треугольнике
+            // Массив сылок на текстуры задан чeрез интерфейс Unity
+            // Никаких проверок на пустые и невалидные переменные мы не делаем, рассчитываем, что дизайнер не накосячит
+            ansverTriangle.GetComponent<MeshRenderer>().material.mainTexture = ansvers[Random.Range(0, ansvers.Length)];
+            getRandomAnsver = false; // Ответ получен
+        }
+
+    }
+}
